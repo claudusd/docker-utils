b3d611f86a80005c46887a42a691118d3f4ba0e3
Claude Dioudonnat <cdioudonnat@itnetwork.fr>
2015-06-11 10:05:16 +0200

    Add command to mysql script to create database

--- mysql.sh
+++ mysql.sh
@@ -15,6 +15,34 @@ function restartContainer() {
     docker restart default_mysql;
 }
 
+function createDatabase()   {
+    if [ -z "$1" ]; then
+        echo "Your must defined a database name";
+        exit 0;
+    fi
+    
+    if [ -z "$2" ]; then
+        DATABASE_USER=$1;
+        DATABASE_USER_PASSWORD=$1;
+    else
+        DATABASE_USER=$2;
+        if [ -z "$3" ]; then
+            DATABASE_USER_PASSWORD=$2;
+       else 
+            DATABASE_USER_PASSWORD=$3;
+        fi
+    fi
+    
+    read -r -d '' VARIABLE <<- EOM
+CREATE DATABASE IF NOT EXISTS \`${1}\` ;
+CREATE USER '$DATABASE_USER'@'%' IDENTIFIED BY '$DATABASE_USER_PASSWORD' ;
+GRANT ALL ON \`${1}\`.* TO '$DATABASE_USER'@'%' ;
+FLUSH PRIVILEGES ;
+EOM
+
+    docker exec -t default_mysql mysql -u root -pitn -e "$VARIABLE" 2> /dev/null
+}
+
 case "$1" in
 
 "run" )
@@ -45,6 +73,20 @@ case "$1" in
         echo "already stop";
     fi  
 
+    ;;
+"create_database" )
+    RUNNING=$(docker inspect --format="{{ .State.Running }}" default_mysql 2> /dev/null)
+    if [ $? -eq 1 ]; then
+        echo "do run";
+    fi
+
+    if [ "$RUNNING" == "true" ]; then
+        createDatabase $2 $3 $4;              
+    fi
+
+    if [ "$RUNNING" == "false" ]; then
+        echo "do run";  
+    fi
     ;;
 * )
     echo "run, stop";

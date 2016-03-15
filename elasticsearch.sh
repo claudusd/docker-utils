043597c33b2e3770ba0be6f1e680980a22b16529
Claude Dioudonnat <cdioudonnat@itnetwork.fr>
2016-03-15 15:22:41 +0100

    Add config file for elasticsearch container

--- elasticsearch.sh
+++ elasticsearch.sh
@@ -1,7 +1,9 @@
 #/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 function run() {
-    docker run -d -p 9200:9200 -p 9300:9300 --name="default_elasticsearch" elasticsearch:2.1 2> /dev/null
+    docker run -d -p 9200:9200 -p 9300:9300 --name="default_elasticsearch" -v "$DIR/config/elasticsearch":/usr/share/elasticsearch/config elasticsearch:2.1 2> /dev/null
     if [ $? -eq 1 ]; then
         restartContainer;
     fi

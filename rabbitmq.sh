f4b535854fcec351a244579d06f075c75d79d338
Claude Dioudonnat <cdioudonnat@itnetwork.fr>
2017-04-24 14:02:17 +0200

    Add script to run a rabbitmq

---
+++ rabbitmq.sh
@@ -0,0 +1,32 @@
+#/bin/bash
+
+function run() {
+    docker run -d -p 5672:5672 --name="default_rabbitmq" rabbitmq:3.6.6 2> /dev/null
+    if [ $? -eq 1]; then
+        restartContainer;       
+    fi  
+}
+
+function restartContainer() {
+    docker restart default_rabbitmq;   
+}
+
+case "$1" in
+"run") 
+    RUNNING=$(docker inspect --format="{{ .State.Running }}" default_rabbitmq 2> /dev/null)
+    if [ $? -eq 1 ]; then
+        run;
+    fi
+    
+    if [ "$RUNNING" == "true" ]; then
+        echo "already running"
+    fi
+    
+    if [ "$RUNNING" == "false" ]; then
+        run;
+    fi
+    ;;
+* )
+    echo "run "
+    ;;
+esac
